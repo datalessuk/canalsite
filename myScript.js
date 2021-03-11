c9e9e684df958f7d227c295a716f9d6557c2eef4
datalessuk <dataless@gmail.com>
2021-03-11 17:39:42 +0000

    adding language support

--- myScript.js
+++ myScript.js
@@ -1,13 +1,37 @@
 
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
+    
     var instances = M.Dropdown.init(elems, {});
+    
+   
+    
+    
   });
+  
+
+
 
   document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {});
+    let elems = document.querySelectorAll('.sidenav');
+    let instances = M.Sidenav.init(elems, {});
   });
 
-  // Or with jQuery
 
+  let x = document.querySelector('#dropdown1');
+
+  console.log(x.value);
+  
+  //function getLanguageCode(){
+
+    //let x = document.getElementById('french');
+    //x.addEventListener('click',languagefr);
+  
+//  }
+
+  //let x = document.getElementById('french');
+  //x.addEventListener('click',languagefr);
+  
+//  function languagefr(){
+  //  window.alert('french');
+  //}
