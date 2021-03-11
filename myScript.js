7dcf60d26114c5e8f9940749b0bca550f5bf2192
datalessuk <dataless@gmail.com>
2021-03-11 16:10:57 +0000

    mobile view updates

--- myScript.js
+++ myScript.js
@@ -3,3 +3,11 @@
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, {});
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+  });
+
+  // Or with jQuery
+
